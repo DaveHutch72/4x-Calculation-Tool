7edc45a2f71f24aebc8f27b64cfcc856c2521b32
David <dhutchis68516@gmail.com>
2020-06-08 08:16:08 -0500

    update to loops and test

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,11 +8,12 @@ class CLI
     def menu
         puts "Would you like to use the tool, look through different types of currency or stop the program?"
         puts "Type 1 to use the tool, 2 to stop program."
+        loop {
         new_input = gets.strip
         if new_input == "1" 
             x = "1"
             while x == "1"
-                puts "Enter the date in the YEAR-MN-DY format"
+                puts "Enter the date in the YYYY-MM-DD format"
                 three_digit_codes
                 date_choice
                 puts "Please enter the index number in reference to the three digit currency code that you wish to use."
@@ -23,24 +24,27 @@ class CLI
             x = gets.strip
             end
             puts "Have a nice day!"
+            break
         elsif new_input == "2"
             puts "Have a nice day!"
+            break
         else
-            puts "That was invalid and this is under construction to send you back to the top."
-
+            puts "That was invalid let me ask ya again. Remember to press 1 to use the tool or 2 to exit the program."
+            sleep 1
         end
+        }
     end
 
     def date_choice
         new_regex = /\A[0-9]{4}-[0-1][0-9]-[0-3][0-9]\z/
         date_input = gets.strip
-        binding.pry
         unless (date_input =~ new_regex) == 0
             puts "Please attempt the date again. Remember (YYYY-MM-DD) format."
             date_input = gets.strip
         end
         @date = date_input
     end
+
 
     def currency_choice
         rate_choice = gets.strip.to_i - 1
